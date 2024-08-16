75032c966ec19a0ac40aa737eb5ff6db1667898f
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2024-08-15 22:47:53 -0300

    Add files via upload

---
+++ mais_alto.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int h_A, h_F, anos;
+
+	h_A = 150;
+	h_F = 110;
+	anos = 0;
+
+	while(h_F<=h_A){
+		h_A += 2;
+		h_F += 3;
+		anos++;
+		}
+
+	printf("%d", anos);
+
+	return 0;
+}
